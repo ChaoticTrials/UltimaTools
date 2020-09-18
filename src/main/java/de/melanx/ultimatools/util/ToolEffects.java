c66b90b543d7dc251923cfec1553582a565424c4
MelanX <info@melanx.de>
2020-09-18 15:16:32 +0200

    vanilla ores -> all ores
    no specific chance anymore

--- src/main/java/de/melanx/ultimatools/util/ToolEffects.java
+++ src/main/java/de/melanx/ultimatools/util/ToolEffects.java
@@ -2,7 +2,6 @@ package de.melanx.ultimatools.util;
 
 import com.google.common.collect.ImmutableList;
 import de.melanx.ultimatools.lib.Function5;
-import net.minecraft.advancements.CriteriaTriggers;
 import net.minecraft.block.*;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
@@ -11,7 +10,6 @@ import net.minecraft.entity.SpawnReason;
 import net.minecraft.entity.passive.AnimalEntity;
 import net.minecraft.entity.passive.WaterMobEntity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.fluid.Fluid;
 import net.minecraft.fluid.Fluids;
 import net.minecraft.item.BoneMealItem;
@@ -21,12 +19,12 @@ import net.minecraft.particles.ParticleTypes;
 import net.minecraft.potion.EffectInstance;
 import net.minecraft.potion.Effects;
 import net.minecraft.state.properties.BlockStateProperties;
-import net.minecraft.stats.Stats;
 import net.minecraft.tags.FluidTags;
 import net.minecraft.util.*;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IServerWorld;
 import net.minecraft.world.World;
+import net.minecraftforge.common.Tags;
 
 import java.util.Collections;
 import java.util.List;
@@ -200,23 +198,7 @@ public class ToolEffects {
         if (world.getBlockState(pos).getBlock() == Blocks.COBBLESTONE
                 || world.getBlockState(pos).getBlock() == Blocks.STONE) {
 
-            int x = world.rand.nextInt(100);
-            Block block;
-
-            if (x <= 49) {
-                block = Blocks.IRON_ORE;
-            } else if (x <= 74) {
-                block = Blocks.GOLD_ORE;
-            } else if (x <= 79) {
-                block = Blocks.DIAMOND_ORE;
-            } else if (x <= 89) {
-                block = Blocks.REDSTONE_ORE;
-            } else if (x <= 98) {
-                block = Blocks.LAPIS_ORE;
-            } else {
-                block = Blocks.EMERALD_ORE;
-            }
-
+            Block block = Tags.Blocks.ORES.getRandomElement(world.rand);
             BlockState state = block.getDefaultState();
             SoundType sound = state.getSoundType();
             world.playSound(null, pos, sound.getPlaceSound(), SoundCategory.BLOCKS, (sound.getVolume() + 1.0F) / 2.0F, sound.getPitch() * 0.8F);
