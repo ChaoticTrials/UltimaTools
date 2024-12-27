3cfe500ee3ad4ee5635e0fa2eaec54675cd88f22
MelanX <info@melanx.de>
2024-12-27 14:50:03 +0100

    Port to 1.21.4

--- src/main/java/de/melanx/ultimatools/data/DataHandler.java
+++ src/main/java/de/melanx/ultimatools/data/DataHandler.java
@@ -4,19 +4,14 @@ import de.melanx.ultimatools.SkyblockUltimaTools;
 import net.minecraft.data.DataGenerator;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
-import net.neoforged.neoforge.common.data.ExistingFileHelper;
 import net.neoforged.neoforge.data.event.GatherDataEvent;
 
 @EventBusSubscriber(modid = SkyblockUltimaTools.MODID, bus = EventBusSubscriber.Bus.MOD)
 public class DataHandler {
 
     @SubscribeEvent
-    public static void gatherData(GatherDataEvent event) {
+    public static void gatherData(GatherDataEvent.Client event) {
         DataGenerator gen = event.getGenerator();
-        ExistingFileHelper helper = event.getExistingFileHelper();
-
-        if (event.includeClient()) {
-            gen.addProvider(true, new ItemModels(gen, helper));
-        }
+        gen.addProvider(true, new ItemModels(gen));
     }
 }
--- src/main/java/de/melanx/ultimatools/data/ItemModels.java
+++ src/main/java/de/melanx/ultimatools/data/ItemModels.java
@@ -2,32 +2,27 @@ package de.melanx.ultimatools.data;
 
 import de.melanx.ultimatools.SkyblockUltimaTools;
 import de.melanx.ultimatools.item.Registration;
-import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.client.data.models.BlockModelGenerators;
+import net.minecraft.client.data.models.ItemModelGenerators;
+import net.minecraft.client.data.models.ModelProvider;
+import net.minecraft.client.data.models.model.ModelTemplates;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.world.item.Item;
-import net.neoforged.neoforge.client.model.generators.ItemModelProvider;
-import net.neoforged.neoforge.common.data.ExistingFileHelper;
 import net.neoforged.neoforge.registries.DeferredHolder;
 
 import javax.annotation.Nonnull;
 
-public class ItemModels extends ItemModelProvider {
+public class ItemModels extends ModelProvider {
 
-    public ItemModels(DataGenerator gen, ExistingFileHelper helper) {
-        super(gen.getPackOutput(), SkyblockUltimaTools.MODID, helper);
+    public ItemModels(DataGenerator gen) {
+        super(gen.getPackOutput(), SkyblockUltimaTools.MODID);
     }
 
     @Override
-    protected void registerModels() {
-        for (DeferredHolder<Item, ?> item : Registration.ITEMS.getEntries())
-            this.generateItem(item.get());
-    }
-
-    private void generateItem(Item item) {
-        @SuppressWarnings("ConstantConditions")
-        String path = BuiltInRegistries.ITEM.getKey(item).getPath();
-        this.getBuilder(path).parent(this.getExistingFile(this.mcLoc("item/handheld")))
-                .texture("layer0", "item/" + path);
+    protected void registerModels(BlockModelGenerators blockModels, ItemModelGenerators itemModels) {
+        for (DeferredHolder<Item, ?> item : Registration.ITEMS.getEntries()) {
+            itemModels.generateFlatItem(item.get(), ModelTemplates.FLAT_HANDHELD_ITEM);
+        }
     }
 
     @Nonnull
